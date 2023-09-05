e9a01e5bc29c0077afdeb5ae435b8c29014061e4
Anisio Aleixo <anisio.junior@zanthus.com.br>
2023-09-04 21:56:56 -0300

    feat: insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import {SafeAreaView, Text} from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 40px;
+  color: blue;
+`;
 
 function App() {
   return (
     <SafeAreaView>
       <Text>Olá</Text>
+      <TextNew>Novo Texto</TextNew>
     </SafeAreaView>
   );
 }
